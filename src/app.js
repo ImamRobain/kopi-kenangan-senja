6bfc29ec25b949780c30f998bee7a24045838e7f
Imam Robain <imamrobain16@gmail.com>
2024-07-08 22:52:14 +0700

    Refactor pricing to use dynamic keys in Alpine.js, make quantity badge in shopping cart

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
-      { id: 1, name: "Robusta Brazil", img: "P1.jpg", price: "Rp. 20.000" },
-      { id: 2, name: "Arabica Blend", img: "P2.jpg", price: "Rp. 25.000" },
-      { id: 3, name: "Primo Passo", img: "P3.jpg", price: "Rp. 30.000" },
-      { id: 4, name: "Aceh Gayo", img: "P4.jpg", price: "Rp. 35.000" },
-      { id: 5, name: "Sumatra Mandheling", img: "P5.jpg", price: "Rp. 40.000" },
+      { id: 1, name: "Robusta Brazil", img: "P1.jpg", price: 20000 },
+      { id: 2, name: "Arabica Blend", img: "P2.jpg", price: 25000 },
+      { id: 3, name: "Primo Passo", img: "P3.jpg", price: 30000 },
+      { id: 4, name: "Aceh Gayo", img: "P4.jpg", price: 35000 },
+      { id: 5, name: "Sumatra Mandheling", img: "P5.jpg", price: 40000 },
     ],
   }));
+  //
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      this.items.push(newItem);
+      this.quantity++;
+      this.total += newItem.price;
+      console.log(this.total);
+    },
+  });
 });
+
+// Konversi ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};

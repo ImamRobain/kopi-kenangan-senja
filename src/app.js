3a105c18d72e32ecd7f44572e2cf71330a6e97de
Imam Robain <imamrobain16@gmail.com>
2024-07-11 23:58:04 +0700

    Add shopping cart feature

--- src/app.js
+++ src/app.js
@@ -1,28 +1,116 @@
+// document.addEventListener("alpine:init", () => {
+//   Alpine.data("products", () => ({
+//     items: [
+//       { id: 1, name: "Robusta Brazil", img: "P1.jpg", price: 20000 },
+//       { id: 2, name: "Arabica Blend", img: "P2.jpg", price: 25000 },
+//       { id: 3, name: "Primo Passo", img: "P3.jpg", price: 30000 },
+//       { id: 4, name: "Aceh Gayo", img: "P4.jpg", price: 35000 },
+//       { id: 5, name: "Sumatra Mandheling", img: "P5.jpg", price: 40000 },
+//     ],
+//   }));
+//   //
+//   Alpine.store("cart", {
+//     items: [],
+//     total: 0,
+//     quantity: 0,
+//     add(newItem) {
+//       // Apakah ada barang yang sama dicart?
+//       const cartItem = this.items.find((item) => item.id === newItem.id);
+//       if (!cartItem) {
+//         this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+//         this.quantity++;
+//         this.total += newItem.price;
+//       } else {
+//         this.items = this.item.map((item) => {
+//           if (item.id !== newItem.id) {
+//             return item;
+//           } else {
+//             item.quantity++;
+//             item.total = item.price * item.quantity;
+//             // this.quantity++;
+//             // this.total = item.price;
+//             return item;
+//           }
+//         });
+//       }
+//       console.log(this.total);
+//     },
+//   });
+// });
+
+// // Konversi ke Rupiah
+// const rupiah = (number) => {
+//   return new Intl.NumberFormat("id-ID", {
+//     style: "currency",
+//     currency: "IDR",
+//     minimumFractionDigits: 0,
+//   }).format(number);
+// };
+
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
       { id: 1, name: "Robusta Brazil", img: "P1.jpg", price: 20000 },
       { id: 2, name: "Arabica Blend", img: "P2.jpg", price: 25000 },
       { id: 3, name: "Primo Passo", img: "P3.jpg", price: 30000 },
-      { id: 4, name: "Aceh Gayo", img: "P4.jpg", price: 35000 },
+      { id: 4, name: "Acah Gayo", img: "P4.jpg", price: 35000 },
       { id: 5, name: "Sumatra Mandheling", img: "P5.jpg", price: 40000 },
     ],
   }));
-  //
   Alpine.store("cart", {
     items: [],
     total: 0,
     quantity: 0,
     add(newItem) {
-      this.items.push(newItem);
-      this.quantity++;
-      this.total += newItem.price;
+      // Apakah ada barang yang sama?
+
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+      // Jika belum ada
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        this.items = this.items.map((item) => {
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+      console.log(newItem);
+      console.log(this.items);
       console.log(this.total);
+    },
+    remove(id) {
+      const cartItem = this.items.find((item) => item.id === id);
+      if (cartItem.quantity > 1) {
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
     },
   });
 });
 
-// Konversi ke Rupiah
+// Konversi ke rupiah
 const rupiah = (number) => {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",

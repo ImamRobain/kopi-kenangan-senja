179ce4de6cd257d9a1869eea875fbe2fd8caabf2
Imam Robain <imamrobain16@gmail.com>
2024-07-17 23:41:00 +0700

    make validation in shopping cart

--- src/app.js
+++ src/app.js
@@ -1,52 +1,3 @@
-// document.addEventListener("alpine:init", () => {
-//   Alpine.data("products", () => ({
-//     items: [
-//       { id: 1, name: "Robusta Brazil", img: "P1.jpg", price: 20000 },
-//       { id: 2, name: "Arabica Blend", img: "P2.jpg", price: 25000 },
-//       { id: 3, name: "Primo Passo", img: "P3.jpg", price: 30000 },
-//       { id: 4, name: "Aceh Gayo", img: "P4.jpg", price: 35000 },
-//       { id: 5, name: "Sumatra Mandheling", img: "P5.jpg", price: 40000 },
-//     ],
-//   }));
-//   //
-//   Alpine.store("cart", {
-//     items: [],
-//     total: 0,
-//     quantity: 0,
-//     add(newItem) {
-//       // Apakah ada barang yang sama dicart?
-//       const cartItem = this.items.find((item) => item.id === newItem.id);
-//       if (!cartItem) {
-//         this.items.push({ ...newItem, quantity: 1, total: newItem.price });
-//         this.quantity++;
-//         this.total += newItem.price;
-//       } else {
-//         this.items = this.item.map((item) => {
-//           if (item.id !== newItem.id) {
-//             return item;
-//           } else {
-//             item.quantity++;
-//             item.total = item.price * item.quantity;
-//             // this.quantity++;
-//             // this.total = item.price;
-//             return item;
-//           }
-//         });
-//       }
-//       console.log(this.total);
-//     },
-//   });
-// });
-
-// // Konversi ke Rupiah
-// const rupiah = (number) => {
-//   return new Intl.NumberFormat("id-ID", {
-//     style: "currency",
-//     currency: "IDR",
-//     minimumFractionDigits: 0,
-//   }).format(number);
-// };
-
 document.addEventListener("alpine:init", () => {
   Alpine.data("products", () => ({
     items: [
@@ -108,6 +59,24 @@ document.addEventListener("alpine:init", () => {
       }
     },
   });
+});
+
+// Form Validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
 });
 
 // Konversi ke rupiah
